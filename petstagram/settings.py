19ff80c83f939c589f694a6634e3f0964817ec31
Zhivka Yordanova <zhivka.yordanova@mentormate.com>
2024-03-14 23:54:22 +0200

    Update the database set up

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'petstagramapp-database',
         'USER': 'dcuvmmbicb',
         'PASSWORD': '36OWHISIOA32OU22$',
-        'HOST': 'petstagramapp.azurewebsites.net',
+        'HOST': 'petstagramapp-server.postgres.database.azure.com',
         'PORT': '5432',
         'OPTIONS': {
             'sslmode': 'require',  # Or 'verify-ca' or 'verify-full' depending on your security requirements
