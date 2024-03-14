6a2a0248263e4d2262b6452e2c4b42b8a0d90846
Zhivka Yordanova <zhivka.yordanova@mentormate.com>
2024-03-15 00:46:26 +0200

    Html refresh

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -123,15 +123,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media/')
 
 AUTH_USER_MODEL = 'accounts.PetstagramUser'
 
