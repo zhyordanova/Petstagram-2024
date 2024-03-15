a53f63399d9427ebcff24eaef8318b3b4dbadc36
Zhivka Yordanova <zhivka.yordanova@mentormate.com>
2024-03-15 19:57:50 +0200

    settings.py

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
